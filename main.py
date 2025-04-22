9186525cbcdad25d6389a6a8f0e6430ec51ec1d3
SwadheenMishra <144024958+SwadheenMishra@users.noreply.github.com>
2025-04-22 18:43:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,39 @@
 import flet as ft
 import random
 
+def profile(page: ft.Page, id: str = None):
+    page.clean()
+    page.update()
+
 def main(page: ft.Page):
-    page.title = "Images Example"
+    page.title = "Ooga Booga"
     page.theme_mode = ft.ThemeMode.DARK
     page.window_width = 450
     page.window_height = 700
-    page.padding = 50
 
-    images = ft.Column(expand=False, wrap=False, scroll=ft.ScrollMode.HIDDEN, adaptive=True, alignment=ft.MainAxisAlignment.CENTER)
+    images = ft.Column(expand=False, wrap=False, scroll=ft.ScrollMode.HIDDEN, alignment=ft.MainAxisAlignment.CENTER)
 
-    imgFiles = ['1.png', '2.jpg', '1.png', '2.jpg', '1.png', '2.jpg', '1.png']
+    imgFiles = ['1.png', '1.png', '1.png', '1.png', '1.png', '1.png', '1.png']
 
     for i in imgFiles:
-        images.controls.append(ft.Text(f"Name: {random.choice(["BOB", "Bryan", "Britny"])}"))
+        #images.controls.append()
+        name = random.choice(["BOB", "Bryan", "Britny", "Laim", "Jhon", "Josh"])
 
         images.controls.append(
+            ft.Card(
+            content=ft.Container(content=ft.Column([
             ft.Image(
                 src=i,
                 width=300,
                 height=400,
                 fit=ft.ImageFit.CONTAIN,
                 repeat=ft.ImageRepeat.NO_REPEAT,
-                border_radius=ft.border_radius.all(10),
                 gapless_playback=False
             ),
+            ft.Row([ft.Text(f"Name: {name}"), ft.Container(width=65), ft.ElevatedButton(text="View Profile", icon="profile", on_click=lambda a: profile(page, id=name))]),
+            #ft.Container(height=5)
+        ], alignment=ft.MainAxisAlignment.CENTER), padding=15))
         )
-
-        images.controls.append(ft.ElevatedButton())
 
     page.add(ft.Row([images], expand=True, alignment=ft.MainAxisAlignment.CENTER))
     page.update()
